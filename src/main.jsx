7f47da278ca13275b81dde3d62e5c4550d77ee21
JowelislamHabib <jowel@bintofajjal.com>
2026-04-08 01:01:10 +0600

    feat: Implement routing with react-router in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <h2 className="text-2xl text-primary">Hello</h2>
+    <RouterProvider router={router} />,
   </StrictMode>,
 );
